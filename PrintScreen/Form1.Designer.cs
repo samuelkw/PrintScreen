da252586c9b2a2759014ee1516a742924fd3e7f6
samuelkw <samuelkw@gmail.com>
2016-08-23 13:47:52 -0300

    Primeira Versão

---
+++ PrintScreen/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PrintScreen
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnAvancar = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(12, 26);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(22, 18);
+            this.btnVoltar.TabIndex = 0;
+            this.btnVoltar.Text = "<<";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // btnAvancar
+            // 
+            this.btnAvancar.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAvancar.Location = new System.Drawing.Point(40, 26);
+            this.btnAvancar.Name = "btnAvancar";
+            this.btnAvancar.Size = new System.Drawing.Size(22, 18);
+            this.btnAvancar.TabIndex = 1;
+            this.btnAvancar.Text = ">>";
+            this.btnAvancar.UseVisualStyleBackColor = true;
+            this.btnAvancar.Click += new System.EventHandler(this.btnAvancar_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(12, 2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(475, 18);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "PRINTSCREEN";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(69, 26);
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(418, 20);
+            this.txtList.TabIndex = 3;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(13, 51);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 13);
+            this.lblMsg.TabIndex = 4;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 66);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnAvancar);
+            this.Controls.Add(this.btnVoltar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.Text = "PrintScreen by Kazuo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnAvancar;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ PrintScreen/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace PrintScreen
+{
+    public partial class frmPrincipal : Form
+    {
+
+        //[DllImport("User32.dll")]
+        //public static extern IntPtr GetDC(IntPtr hwnd);
+        //[DllImport("User32.dll")]
+        //public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        //public static IntPtr desktopPtr;
+
+        public static DirectoryInfo dirDest;
+        public static DirectoryInfo subdir;
+        public static FileInfo arqImg;
+        public static FileInfo arqDest;
+        public static DirectoryInfo dirList;
+        public static String[] lista;
+        public static FileInfo arqList;
+        public static int idx = 0;
+        public static int tam = 0;
+
+        public static Rectangle resolucao;
+        public static int altura;
+        public static int largura;
+
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            initAppConfigReader();
+            //desktopPtr = GetDC(IntPtr.Zero);
+            lblMsg.Text = "";
+            resolucao = Screen.PrimaryScreen.Bounds;
+            altura = resolucao.Size.Height;
+            largura = resolucao.Size.Width;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "Gerando PrintScreen";
+            txtList.Enabled = false;
+            btnGo.Enabled = false;
+            btnAvancar.Enabled = false;
+            btnVoltar.Enabled = false;
+            String strDir = dirDest.FullName;
+            String strArq="";
+            if (!strDir.EndsWith("\\")) strDir += "\\";
+            strDir += txtList.Text;
+            strArq = txtList.Text;
+            subdir = new DirectoryInfo(strDir);
+            if (!subdir.Exists) subdir.Create();
+            if (!strDir.EndsWith("\\")) strDir += "\\";
+            arqImg = new FileInfo(strDir+strArq+".jpg");
+            int count=0;
+            while (arqImg.Exists)
+            {
+                arqImg = new FileInfo(strDir+strArq+"_"+count+".jpg");
+                count++;
+            }
+            if (prntScr(arqImg))
+            {
+                lblMsg.Text = "PrintScreen gerado em: "+ arqImg.FullName;
+            }
+            else
+            {
+                lblMsg.Text = "Erro ao tentar gerar PrintScreen!";
+            }
+            txtList.Enabled = true;
+            btnGo.Enabled = true;
+            btnAvancar.Enabled = true;
+            btnVoltar.Enabled = true;
+        }
+
+        private void btnAvancar_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "";
+            txtList.Enabled = false;
+            btnGo.Enabled = false;
+            btnAvancar.Enabled = false;
+            btnVoltar.Enabled = false;
+            nextIdx();
+            txtList.Enabled = true;
+            btnGo.Enabled = true;
+            btnAvancar.Enabled = true;
+            btnVoltar.Enabled = true;
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "";
+            txtList.Enabled = false;
+            btnGo.Enabled = false;
+            btnAvancar.Enabled = false;
+            btnVoltar.Enabled = false;
+            prevIdx();
+            txtList.Enabled = true;
+            btnGo.Enabled = true;
+            btnAvancar.Enabled = true;
+            btnVoltar.Enabled = true;
+        }
+
+        public void initAppConfigReader()
+        {
+            System.Configuration.AppSettingsReader appCfg = new System.Configuration.AppSettingsReader();
+            try
+            {
+                String dir0 = appCfg.GetValue("pathDest", typeof(String)).ToString();
+                String dir1 = appCfg.GetValue("dirDest", typeof(String)).ToString();
+                if (!dir0.EndsWith("\\")) dir0 = dir0 + "\\";
+                dirDest = new DirectoryInfo(dir0+dir1);
+                if (!dirDest.Exists) dirDest.Create();
+            }
+            catch { System.Environment.Exit(1); }
+            try
+            {
+                String dir = appCfg.GetValue("pathList", typeof(String)).ToString();
+                String arq = appCfg.GetValue("arqList", typeof(String)).ToString();
+                Application.DoEvents();
+                dirList = new DirectoryInfo(dir);
+                if (!dir.EndsWith("\\")) dir = dir + "\\";
+                arqList = new FileInfo(dir + arq);
+                if (!arqList.Exists) { System.Environment.Exit(1); }
+                Application.DoEvents();
+                lista = System.IO.File.ReadAllLines(arqList.FullName);
+                //System.Threading.Thread.Sleep(2 * 1000);
+                Application.DoEvents();
+                tam = lista.Length;
+                txtList.Text = lista[0];
+            }
+            catch { System.Environment.Exit(1); }
+        }
+
+        public void nextIdx()
+        {
+            idx++;
+            if (idx >= tam)
+            {
+                lblMsg.Text = "Fim da lista, reiniciando a listagem...";
+                idx = 0;
+            }
+            txtList.Text = lista[idx];
+        }
+
+        public void prevIdx()
+        {
+            idx--;
+            if (idx <= 0)
+            {
+                idx = tam-1;
+                lblMsg.Text = "Indo para o último registro da listagem...";
+            }
+            txtList.Text = lista[idx];
+        }
+
+        public bool prntScr(FileInfo arquivo)
+        {
+            try
+            {
+                Bitmap printscreen = new Bitmap(largura, altura);
+                //Bitmap como imagem
+                Graphics graphics = Graphics.FromImage(printscreen as Image);
+                //Pos inicial do bitmap
+                int esquerda = 0;
+                int topo = 0;
+                /*Associção da imagem Bitmap ao objecto graphics, com a informação de onde começa a imagem, e qual tamanho que terá */
+                graphics.CopyFromScreen(esquerda, topo, 0, 0, printscreen.Size);
+                //System.Threading.Thread.Sleep(3 * 1000);
+                Application.DoEvents();
+                //Caminho aonde saltar, o nome da imagem, a extensão e o formato
+                //System.Threading.Thread.Sleep(3 * 1000);
+                printscreen.Save(arquivo.FullName, ImageFormat.Jpeg);
+                System.Threading.Thread.Sleep(3 * 1000);
+                return true;
+            }
+            catch { return false; }
+        }
+
+
+
+    }
+}
